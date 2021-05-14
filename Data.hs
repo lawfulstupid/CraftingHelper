4a49067449eb9703ea7c158ca0824a26a7b943da
Alex Bradley <a.bradley.dm@gmail.com>
2021-05-14 19:44:43 +0100

    RecipeSelector object

--- Data.hs
+++ Data.hs
@@ -3,10 +3,10 @@ module CraftingHelper.Data (
    module CraftingHelper.Data.ItemStack,
    module CraftingHelper.Data.Recipe,
    module CraftingHelper.Data.RecipeBook,
-   module CraftingHelper.Data.RecipeSelection
+   module CraftingHelper.Data.RecipeSelector
 ) where
 
 import CraftingHelper.Data.ItemStack
 import CraftingHelper.Data.Recipe
 import CraftingHelper.Data.RecipeBook
-import CraftingHelper.Data.RecipeSelection
+import CraftingHelper.Data.RecipeSelector
---
+++ Data/RecipeSelector.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE LambdaCase #-}
+
+module CraftingHelper.Data.RecipeSelector where
+
+import CraftingHelper.Data.ItemStack
+import CraftingHelper.Data.Recipe
+import CraftingHelper.Data.RecipeBook
+
+import AbLib.Data.List
+import AbLib.Data.IO
+
+import Control.Monad
+import Data.IORef
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+--------------------------------------------------------------------------------
+
+newtype RecipeSelector = RS (Map Item (IORef RecipeSelectorUnit))
+data RecipeSelectorUnit = RecipeSelectorUnit
+   { options :: [Recipe]
+   , selection :: Maybe Recipe }
+
+--------------------------------------------------------------------------------
+
+makeRecipeSelector :: RecipeBook -> IO RecipeSelector
+makeRecipeSelector b = RS <$> (sequenceMap $ Map.map makeUnit b)
+   where
+   makeUnit :: [Recipe] -> IO (IORef RecipeSelectorUnit)
+   makeUnit rs = newIORef $ RecipeSelectorUnit rs Nothing
+   
+   sequenceMap :: Monad m => Map k (m a) -> m (Map k a)
+   sequenceMap m = Map.fromDistinctAscList <$> (sequence $ map sequence $ Map.assocs m)
+
+selectRecipe :: Item -> RecipeSelector -> IO (Maybe Recipe)
+selectRecipe = getFromMap
+   where
+   getFromMap :: Item -> RecipeSelector -> IO (Maybe Recipe)
+   getFromMap item (RS m) = case Map.lookup item m of
+      Nothing -> pure Nothing
+      Just ref -> do
+         unit <- readIORef ref
+         recipe <- getFromUnit unit
+         writeIORef ref $ RecipeSelectorUnit (options unit) recipe
+         pure recipe
+
+   getFromUnit :: RecipeSelectorUnit -> IO (Maybe Recipe)
+   getFromUnit unit = case selection unit of
+      Just r -> pure $ Just r
+      Nothing -> getFromOptions (options unit)
+      
+   getFromOptions :: [Recipe] -> IO (Maybe Recipe)
+   getFromOptions = \case
+      [] -> pure Nothing
+      [r] -> pure (Just r)
+      recipes -> getFromUser recipes
+   
+   getFromUser :: [Recipe] -> IO (Maybe Recipe)
+   getFromUser recipes = do
+      forM_ (zip [1..] recipes) $ \(n,r) -> putStr ("(" ++ show n ++ ")  ") >> print r
+      putStr "Selection: "
+      k <- getKey
+      let idx = if ('0' <= k && k <= '9') then read [k] - 1 else -1
+      case recipes !? idx of
+         Just r -> pure $ Just r
+         Nothing -> putStrLn "invalid, try again\n" >> getFromUser recipes
